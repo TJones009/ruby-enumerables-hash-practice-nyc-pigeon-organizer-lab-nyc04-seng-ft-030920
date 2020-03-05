30fd66abc0760d3e33810ab80dbf36e7d7d7a8bf
Trevor Jones <tjones009@yahoo.com>
2020-03-05 01:14:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,6 +8,9 @@ def nyc_pigeon_organizer(data)
         if !new_hash[name]
           new_hash[name] = {}
         end
+        
+        if !new_hash[name][key]
+          new_hash[name][key]
    end
   end
 end
